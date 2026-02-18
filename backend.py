1a4dbc638f1c2f840846d4ef2a5327ea587e5216
piklu111 <59930050+piklu111@users.noreply.github.com>
2026-02-19 00:57:33 +0530

    Add files via upload

--- backend.py
+++ backend.py
@@ -4,8 +4,13 @@ from langchain_core.messages import HumanMessage, BaseMessage, AIMessage
 from langchain_openai import ChatOpenAI
 from langgraph.graph.message import add_messages
 from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
+import streamlit as st
 
-checkpointer = MemorySaver()
+conn = sqlite3.connect('chatbot.db', check_same_thread= False)
+
+checkpointer = SqliteSaver(conn = conn)
 
 
 
@@ -26,3 +31,20 @@ def create_graph(api_key,model_name,temp):
     graph.add_edge('chat_node', END)
 
     return graph.compile(checkpointer= checkpointer)
+
+def get_thread_id(user_id):
+    checkpoint_lst = []
+    for checkpoint in checkpointer.list(None):
+        print('checkpoint', checkpoint)
+        user_id = checkpoint.metadata['user_id']
+        print('user_id', user_id)
+        if user_id == st.session_state.user_id:
+            thread_id = checkpoint.config['configurable']['thread_id']
+            if thread_id not in checkpoint_lst:
+                checkpoint_lst.append(thread_id)
+    return checkpoint_lst
+
+def delete_thread(thread_id: str):
+    with conn:
+        conn.execute("DELETE FROM checkpoints WHERE thread_id = ?", (thread_id,))
+        conn.execute("DELETE FROM writes WHERE thread_id = ?", (thread_id,))
